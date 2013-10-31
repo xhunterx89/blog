8c8d24fddca44b827908f9ee9421d0e3b01be771
Tuan Dung <dunghtt@hus.edu.vn>
2013-10-31 13:40:10 +0700

    view user

--- protected/views/site/pages/about.php
+++ protected/views/site/pages/about.php
@@ -1,12 +1,44 @@
 <?php
 /* @var $this SiteController */
 
-$this->pageTitle=Yii::app()->name . ' - About';
+$this->pageTitle=Yii::app()->name . ' - introduction';
 $this->breadcrumbs=array(
-	'About',
+	'introduction',
 );
 ?>
-<h1>About</h1>
 
-<p>This is a "static" page. You may change the content of this page
-by updating the file <code><?php echo __FILE__; ?></code>.</p>
+<script type="text/javascript" src="http://cdn.dev.skype.com/uri/skype-uri.js"></script>
+<script src='<?php echo Yii::app()->baseUrl; ?>/js/introduction.js'></script> 
+
+<div class="row">
+    <div class="row">
+        <h2><span class="introduction" id="about">About</span><h2>
+            <hr/>
+            <div id="about_content">
+            	<h6>
+                <p>
+                    Blog is made by <span class="secondary label">PHP Trainning Team</span>
+                </br>
+                    It is written in PHP, using <a href="http://www.yiiframework.com/">Yii Framework</a> 
+                </p>
+                </h6>  
+            </div>
+    </div>
+
+    <div class="row">
+        <h2><span class="introduction" id="contact">Contact</span><h2>                     
+        <hr/>
+        <div id="contact_content">
+            <p style="display: inline">
+            	<h6>
+                <span style="float:left"><span class="primary label">Blog</span> was developed in a very short time and
+                it has not been tested carefully yet.
+                <br/>If you have any ideas or found any bugs, please feel free to contact me.  
+                You can <a href="mailto:hoang.thi.tuan.dung@framgia.com">send me an email</a>. </span> 
+                <br/>
+            </h6>
+            </p>
+        </div>
+    </div>
+</div>
+
