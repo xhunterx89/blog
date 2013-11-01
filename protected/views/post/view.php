928af07718fa3ecf6f1243a8319f714dd41c6674
daolm <daolm89@gmail.com>
2013-11-01 13:40:10 +0700

    add css

--- protected/views/post/view.php
+++ protected/views/post/view.php
@@ -1,21 +1,3 @@
-<?php
-/* @var $this PostController */
-/* @var $model Post */
-
-$this->breadcrumbs=array(
-	'Posts'=>array('index'),
-	$model->title,
-);
-
-// $this->menu=array(
-// 	array('label'=>'List Post', 'url'=>array('index')),
-// 	array('label'=>'Create Post', 'url'=>array('create')),
-// 	array('label'=>'Update Post', 'url'=>array('update', 'id'=>$model->id)),
-// 	array('label'=>'Delete Post', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-// 	array('label'=>'Manage Post', 'url'=>array('admin')),
-// );
-?>
-
 <h1>View Post #<?php echo $model->id; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
@@ -28,3 +10,32 @@ $this->breadcrumbs=array(
 		'date_create',
 	),
 )); ?>
+
+<?php
+/* @var $this PostController */
+/* @var $model Post */
+
+$this->breadcrumbs=array(
+	'Posts'=>array('index'),
+	$model->title,
+);
+ if(!Yii::app()->user->isGuest){
+    $this->widget('zii.widgets.CMenu',array(
+     'activeCssClass'=>'active',
+     'activateParents'=>true,
+     'items'=>array(
+       array(
+         'linkOptions'=>array('id'=>'menuCompany'),
+         'itemOptions'=>array('id'=>'itemCompany'),
+         'items'=>array(
+           array('label'=>'List Post', 'url'=>array('index')),
+           array('label'=>'Create Post', 'url'=>array('create')),
+           array('label'=>'Update Post', 'url'=>array('update','id'=>$model->id)),
+           array('label'=>'Delete Post', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
+           array('label'=>'Manage Post', 'url'=>array('admin')),
+         ),
+       ),
+       ),)
+   );
+ }
+?>
